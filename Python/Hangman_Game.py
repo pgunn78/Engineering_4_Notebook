d1a40671ae56ddfa96dbf4a3a860a39ca5b5a42e
pgunn78 <pgunn78@charlottesvilleschools.org>
2019-09-19 11:42:07 -0400

    yes this is it

--- Python/Hangman_Game.py
+++ Python/Hangman_Game.py
@@ -1,2 +1,39 @@
 #Writtin by Zane&Paxton
-answer = input("Whats the Word")
+guessCount = 5
+lettersGuessed = []
+sentence = input("Whats the Word: ")
+print("\n" * 50)
+sentence = sentence.split()
+
+while (guessCount != 0):
+    for word in sentence:
+        for letter in word:
+            print("_", end = '')
+        print(" ", end = '')
+print("\n")
+guess = input("Guess: ")
+
+def letterCheck (guess,sentence):
+    for letter in sentence:
+        if (guess == letter):
+            print("thats right")
+            print("there is"+ amountLetters + guess)
+        #letter you guessed correct and how many of that letter there are
+        elif (guess != letter):
+             guessCount - 1
+             print("no " + guess)
+        lettersGuessed.append(guess)
+
+if (guessCount == 5):
+    print("amount of guesses left" + guessCount)
+if (guessCount == 4):
+    print("amount of guesses left" + guessCount)
+if (guessCount == 3):
+    print("amount of guesses left" + guessCount)
+if (guessCount == 2):
+    print("amount of guesses left" + guessCount)
+if (guessCount == 1):
+    print("amount of guesses left" + guessCount)
+if (guessCount == 0):
+    print("\n" * 50)
+    print("You Lose")
