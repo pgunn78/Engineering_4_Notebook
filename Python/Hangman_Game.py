561f5e717b5d39cef7773e7052b9e9dc97fcc935
pgunn78 <pgunn78@charlottesvilleschools.org>
2019-09-20 11:39:35 -0400

    made changes

--- Python/Hangman_Game.py
+++ Python/Hangman_Game.py
@@ -7,33 +7,35 @@ sentence = sentence.split()
 
 while (guessCount != 0):
     for word in sentence:
-        for letter in word:
-            print("_", end = '')
-        print(" ", end = '')
+        if guess in word:
+            print(guess, end = '')
+        elif print("_")
+    print(" ", end = '')
 print("\n")
-guess = input("Guess: ")
+    guess = input("Guess: ")
 
 def letterCheck (guess,sentence):
-    for letter in sentence:
-        if (guess == letter):
-            print("thats right")
-            print("there is"+ amountLetters + guess)
+    for word in sentence:
+        for letter in word:
+            if (guess == letter):
+                print("thats right")
+                print("there is"+ amountLetters + guess)
         #letter you guessed correct and how many of that letter there are
-        elif (guess != letter):
-             guessCount - 1
-             print("no " + guess)
-        lettersGuessed.append(guess)
+            elif (guess != letter):
+                 guessCount - 1
+                 print("no " + guess)
+            lettersGuessed.append(guess)
 
 if (guessCount == 5):
-    print("amount of guesses left" + guessCount)
+    print("amount of guesses left " + guessCount)
 if (guessCount == 4):
-    print("amount of guesses left" + guessCount)
+    print("amount of guesses left " + guessCount)
 if (guessCount == 3):
-    print("amount of guesses left" + guessCount)
+    print("amount of guesses left " + guessCount)
 if (guessCount == 2):
-    print("amount of guesses left" + guessCount)
+    print("amount of guesses left " + guessCount)
 if (guessCount == 1):
-    print("amount of guesses left" + guessCount)
+    print("amount of guesses left " + guessCount)
 if (guessCount == 0):
     print("\n" * 50)
     print("You Lose")
---
+++ Python/wirdtst.py
@@ -0,0 +1,3 @@
+list = ['a','b','c']
+list.remove('b')
+print(list)
