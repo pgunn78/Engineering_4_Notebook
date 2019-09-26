7be7fd38eda1c5094e6ffaa67fc9e39f612bd761
pgunn78 <pgunn78@charlottesvilleschools.org>
2019-09-26 11:42:07 -0400

    made changes to hangman

--- Python/Hangman_Game.py
+++ Python/Hangman_Game.py
@@ -1,11 +1,12 @@
 #Writtin by Zane&Paxton
 guessCount = 5
 guessCountstr = str(guessCount)
-lettersGuessed = []
+lettersGuessed = ''
 lettersGuessedstr = ''.join(str(e) for e in lettersGuessed)
-sentence = input("Whats the Word (only lower case please): ")
+word = input("Whats the Word (only lower case please): ")
 print("\n" * 50)
-sentence = sentence.split()
+
+
 if (guessCount == 5):
     print("amount of guesses left " + guessCountstr)
 if (guessCount == 4):
@@ -20,28 +21,24 @@ if (guessCount == 0):
     print("\n" * 50)
     print("You Lose")
 
-for word in sentence:
-    if lettersGuessed in word:
-        print(lettersGuessedstr, end = '')
-    elif lettersGuessed not in word:
-        print("_")
-print(" ", end = '')
-print("\n")
 
 while (guessCount > 0):
+    gamestate = 0
     guess = input("Guess: ")
     if guess not in lettersGuessed:
-        for word in sentence:
-            if guess in word:
+        for letter in word:
+            if letter in lettersGuessed:
                 print("Thats Right")
                 print("there is a " + guess)
-            elif guess not in word:
-                guessCount-1
+            elif letter not in lettersGuessed:
+                guessCount = guessCount-1
                 print("no " + guess)
-            lettersGuessed.append(guess)
+        lettersGuessed = lettersGuessed + guess
     elif guess in lettersGuessed:
         print("you already guessed " + guess)
-
-    
-#def letterCheck (guess,sentence):
-
+    for letter in word:
+        if letter in lettersGuessed:
+            print(letter, end = '')
+        elif letter not in lettersGuessed:
+            print("_", end = '')
+    print("\n")
