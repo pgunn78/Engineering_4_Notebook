d510cb8216da10afb8b546b539751e7f56656ce3
pgunn78 <pgunn78@charlottesvilleschools.org>
2019-09-27 11:41:40 -0400

    made changes

--- Python/Hangman_Game.py
+++ Python/Hangman_Game.py
@@ -1,38 +1,25 @@
 #Writtin by Zane&Paxton
 guessCount = 5
-guessCountstr = str(guessCount)
 lettersGuessed = ''
-lettersGuessedstr = ''.join(str(e) for e in lettersGuessed)
 word = input("Whats the Word (only lower case please): ")
 print("\n" * 50)
 
-
-if (guessCount == 5):
-    print("amount of guesses left " + guessCountstr)
-if (guessCount == 4):
-    print("amount of guesses left " + guessCountstr)
-if (guessCount == 3):
-    print("amount of guesses left " + guessCountstr)
-if (guessCount == 2):
-    print("amount of guesses left " + guessCountstr)
-if (guessCount == 1):
-    print("amount of guesses left " + guessCountstr)
-if (guessCount == 0):
-    print("\n" * 50)
-    print("You Lose")
-
-
 while (guessCount > 0):
-    gamestate = 0
+    gameState = 0
+    lettersCorrect = 0
+    print("amount of guesses left ", + guessCount)
     guess = input("Guess: ")
     if guess not in lettersGuessed:
         for letter in word:
-            if letter in lettersGuessed:
-                print("Thats Right")
-                print("there is a " + guess)
-            elif letter not in lettersGuessed:
-                guessCount = guessCount-1
-                print("no " + guess)
+            gameState = gameState + 1
+            if letter in guess:
+                lettersCorrect = lettersCorrect + 1
+                gameState = gameState - 1
+        if lettersCorrect > 0:
+                print("correct there is a " + guess)
+        elif lettersCorrect == 0:
+                print("wrong there is no " + guess)
+                guessCount = guessCount - 1
         lettersGuessed = lettersGuessed + guess
     elif guess in lettersGuessed:
         print("you already guessed " + guess)
@@ -42,3 +29,10 @@ while (guessCount > 0):
         elif letter not in lettersGuessed:
             print("_", end = '')
     print("\n")
+    if gameState == 0:
+        print("You Win!")
+        break
+    print("\n")
+    
+if guessCount == 0:
+    print("You Lose")
