49394fd4568576e6f882d248111a966c423adb75
pgunn78 <pgunn78@charlottesvilleschools.org>
2019-10-08 12:04:43 -0400

    worked on Flask

--- Python/Flask/gpio/app.py
+++ Python/Flask/gpio/app.py
@@ -9,33 +9,19 @@ GPIO.setup(22,GPIO.OUT)
 
 app = Flask (__name__)
 
-#blue
 @app.route("/", methods=["GET","Post"])
 def index():
-	if request.method == "POSTB":
+	if request.method == "POST":
 		GPIO.output(17,GPIO.HIGH)
 		msg = request.form.get("submitBtnB")
-	else:
-		GPIO.output(17,GPIO.LOW)
-		msg = "No click yet."
-	return render_template("index.html", msg=msg)
-#red
-@app.route("/", methods=["GET","Post"])
-def index():
-        if request.method == "POSTR":
                 GPIO.output(27,GPIO.HIGH)
                 msg = request.form.get("submitBtnR")
-        else:
-                GPIO.output(27,GPIO.LOW)
-                msg = "No click yet."
-        return render_template("index.html", msg=msg)
-#green
-@app.route("/", methods=["GET","Post"])
-def index():
-        if request.method == "POSTG":
                 GPIO.output(22,GPIO.HIGH)
                 msg = request.form.get("submitBtnG")
-        else:
+
+	else:
+		GPIO.output(17,GPIO.LOW)
+                GPIO.output(27,GPIO.LOW)
                 GPIO.output(22,GPIO.LOW)
                 msg = "No click yet."
         return render_template("index.html", msg=msg)
