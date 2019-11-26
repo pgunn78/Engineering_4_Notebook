56f33a19612e8d2db3ac9642f1dc12756a59e30d
pgunn78 <54992871+pgunn78@users.noreply.github.com>
2019-11-26 11:18:10 -0500

    commented code

--- Python/Calculator.py
+++ Python/Calculator.py
@@ -1,9 +1,9 @@
 #Writtin by Zane&Paxton
 
-a = float(input('1st num: '))
+a = float(input('1st num: ')) #floating integers wich can be decieded by the user
 b = float(input('2nd num: '))
 
-def doMath(a,b,c):
+def doMath(a,b,c): #a nice diy function that does all the hard math for us
     if (c == 1):
         result = a+b
     elif (c == 2):
@@ -16,7 +16,7 @@ def doMath(a,b,c):
         result = a%b
     return str(result)
 
-print("sum:\t\t" + doMath(a,b,1))
+print("sum:\t\t" + doMath(a,b,1))  #prints out all the necesary values for the user to see
 print("Difference:\t\t" + doMath(a,b,2))
 print("Product:\t\t" + doMath(a,b,3))
 print("Quotient:\t\t" + doMath(a,b,4))
