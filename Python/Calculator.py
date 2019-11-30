c063e0ff1218180f652005492a0fe7d0e81ea04d
pgunn78 <54992871+pgunn78@users.noreply.github.com>
2019-11-30 18:43:33 -0500

    Update Calculator.py

--- Python/Calculator.py
+++ Python/Calculator.py
@@ -1,9 +1,9 @@
 #Writtin by Zane&Paxton
 
-a = float(input('1st num: ')) #floating integers wich can be decieded by the user
+a = float(input('1st num: ')) 
 b = float(input('2nd num: '))
 
-def doMath(a,b,c): #a nice diy function that does all the hard math for us
+def doMath(a,b,c): #Defines the DoMath function to find the sum, difference, product, quotient, and modulo between the two numbers
     if (c == 1):
         result = a+b
     elif (c == 2):
@@ -16,7 +16,7 @@ def doMath(a,b,c): #a nice diy function that does all the hard math for us
         result = a%b
     return str(result)
 
-print("sum:\t\t" + doMath(a,b,1))  #prints out all the necesary values for the user to see
+print("sum:\t\t" + doMath(a,b,1))  #prints out all the necesary values
 print("Difference:\t\t" + doMath(a,b,2))
 print("Product:\t\t" + doMath(a,b,3))
 print("Quotient:\t\t" + doMath(a,b,4))
