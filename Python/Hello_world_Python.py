31ed9a23510c01a5157e211d86bc445630c5e69e
pgunn78 <54992871+pgunn78@users.noreply.github.com>
2019-11-30 18:35:48 -0500

    Update Hello_world_Python.py

--- Python/Hello_world_Python.py
+++ Python/Hello_world_Python.py
@@ -8,5 +8,5 @@ choice = ''
 print("press enter to roll dice press x then enter to exit program")
 
 while(choice != "x"):
-    print(random.randint(1,6))
-    choice = input() # generataes a randome number between one and six
+    print(random.randint(1,6))# generataes a randome number between one and six and print it
+    choice = input() 
